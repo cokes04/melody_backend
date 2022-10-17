d7f169fff10b510f25b70c1debe74840725582df
cokes04 <devokes04@naver.com>
2022-10-17 12:44:16 +0900

    설정 변경

--- src/main/java/com/melody/melody/MelodyApplication.java
+++ src/main/java/com/melody/melody/MelodyApplication.java
@@ -3,8 +3,16 @@ package com.melody.melody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication(scanBasePackages = {"com.melody.melody"})
 public class MelodyApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MelodyApplication.class, args);
