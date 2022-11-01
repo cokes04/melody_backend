f27ea9d20610ad06fe831ae32629cfbfc3b25b95
cokes04 <devokes04@naver.com>
2022-11-01 19:58:15 +0900

    lambda 관련 테스트 Mocking 방식으로 변경

--- src/test/java/com/melody/melody/adapter/aws/LambdaEmotionClassifierTest.java
+++ src/test/java/com/melody/melody/adapter/aws/LambdaEmotionClassifierTest.java
@@ -1,27 +1,63 @@
 package com.melody.melody.adapter.aws;
 
-import com.melody.melody.config.JasyptConfig;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.melody.melody.adapter.aws.response.EmotionMapping;
+import com.melody.melody.adapter.aws.response.LambdaEmotionClassifitionResponse;
 import com.melody.melody.domain.model.Emotion;
 import com.melody.melody.domain.model.Music;
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-@ActiveProfiles(profiles = "dev")
-@EnableEncryptableProperties
-@ContextConfiguration(classes = {JasyptConfig.class})
-@SpringBootTest(classes = {LambdaEmotionClassifier.class})
+@ExtendWith(MockitoExtension.class)
 class LambdaEmotionClassifierTest {
 
-    @Autowired
     private LambdaEmotionClassifier classifier;
 
+    private ObjectMapper objectMapper;
+    private MockWebServer mockWebServer;
+    private Dispatcher dispatcher;
+    private MockResponse response;
+
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        mockWebServer = new MockWebServer();
+
+        response = new MockResponse()
+                .addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.OK.value())
+        .setBody(
+                objectMapper.writeValueAsString(
+                        LambdaEmotionClassifitionResponse.builder().emotion(EmotionMapping.of(Emotion.RELAXED).getClassification()).build()
+                )
+        );
+
+        dispatcher = new Dispatcher() {
+            @NotNull
+            public MockResponse dispatch(@NotNull RecordedRequest request) {
+                return response;
+            }
+        };
+
+        mockWebServer.setDispatcher(dispatcher);
+
+        classifier = new LambdaEmotionClassifier();
+        String mockServerUrl = mockWebServer.url("/").toString();
+        ReflectionTestUtils.setField(classifier, "uri", mockServerUrl);
+    }
     @Test
     void execute_returnEmotion() {
         Music.Explanation caption = new Music.Explanation("안녕하다 행복하다");
--- src/test/java/com/melody/melody/adapter/aws/LambdaMusicGeneratorTest.java
+++ src/test/java/com/melody/melody/adapter/aws/LambdaMusicGeneratorTest.java
@@ -1,37 +1,62 @@
 package com.melody.melody.adapter.aws;
 
-import com.melody.melody.config.JasyptConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.melody.melody.domain.model.Emotion;
 import com.melody.melody.domain.model.Music;
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import org.aspectj.lang.annotation.Before;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.function.Function;
 
 import static com.melody.melody.application.service.TestServiceGenerator.*;
 import static com.melody.melody.domain.model.TestDomainGenerator.*;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@ActiveProfiles(profiles = "dev")
-@EnableEncryptableProperties
-@ContextConfiguration(classes = {JasyptConfig.class})
-@SpringBootTest(classes = {LambdaMusicGenerator.class})
+@ExtendWith(MockitoExtension.class)
 class LambdaMusicGeneratorTest {
 
-    @Autowired
     private LambdaMusicGenerator generator;
+
+    private MockWebServer mockWebServer;
+    private Dispatcher dispatcher;
+    private MockResponse response;
+
+    @BeforeEach
+    void setUp() {
+        mockWebServer = new MockWebServer();
+
+        response = new MockResponse()
+                .addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.OK.value());
+
+         dispatcher = new Dispatcher() {
+            @NotNull
+            public MockResponse dispatch(@NotNull RecordedRequest request) {
+                return response;
+            }
+        };
+
+        mockWebServer.setDispatcher(dispatcher);
+
+        generator = new LambdaMusicGenerator();
+        String mockServerUrl = mockWebServer.url("/").toString();
+        ReflectionTestUtils.setField(generator, "uri", mockServerUrl);
+    }
 
     @Test
     void executeAsync_clientSuccess() {
+
         Music.MusicId musicId = randomMusicId();
         Emotion emotion = randomEmotion();
         int musicLength = randomMusicLength();
